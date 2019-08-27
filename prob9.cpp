ec56a09c46587b7ea9e0c7f074128737c5f39f78
ASVKVINAYAK <53367345+ASVKVINAYAK@users.noreply.github.com>
2019-08-27 15:09:47 +0530

    Update prob9.cpp

--- prob9.cpp
+++ prob9.cpp
@@ -1,12 +1,13 @@
+// OPTIMIZED THE LOOP FROM RUNNING UPTO 1000 TO 500 
 #include<iostream>
 #include<math.h>
 using namespace std;
 int main()
 {
 int i,j,r,c;
-for(i=1;i<1000;i++)
+for(i=1;i<500;i++)
 {
-	for(j=2;j<1000;j++)
+	for(j=2;j<500;j++)
 	{ 
 	c=1000-i-j;
 	if((i*i)+(j*j)==(c*c))
@@ -16,7 +17,7 @@ for(i=1;i<1000;i++)
 	}
 }
 }	
-      cout<<" Triplet Product is "<<r<<endl;	
+      cout<<" Triplet Product is "<<r<<endl;	// OUTPUT IS 31875000
 
 return 0;	
 }
